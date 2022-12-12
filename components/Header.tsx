983e42318d9e98c8d3fa06e4270ad1a052950f48
William M. Tsikata <tsikatawill@gmail.com>
2022-12-12 20:28:58 +0000

    feat: add sanity

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <header className="relative">
+      <Image
+        alt="apple-logo.png"
+        src="http://rb.gy/vsvv2o"
+        width="10"
+        height="10"
+      />
+    </header>
+  );
+};
